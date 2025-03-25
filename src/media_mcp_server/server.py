c55ad44abc3bb998581fc9c06e76e2a802b02a76
rocksun <daijun@gmail.com>
2025-03-25 23:09:31 +0800

    add

--- src/media_mcp_server/server.py
+++ src/media_mcp_server/server.py
@@ -17,6 +17,14 @@ cloudinary.config(
 
 @mcp.tool()
 async def upload_image(image_path: str) -> str:
+    """Uploads an image to Cloudinary.
+
+    Args:
+        image_path: Local file path of the image to upload.
+
+    Returns:
+        str: Secure URL of the uploaded image on Cloudinary CDN.
+    """
     upload_result = uploader.upload(
         image_path,
         transformation=[
@@ -25,11 +33,7 @@ async def upload_image(image_path: str) -> str:
     )
     return upload_result['secure_url']
 
-    # url = f"{NWS_API_BASE}/alerts/active/area/{state}"
-    # data = await make_nws_request(url)
-
-# def main():
-#     print("Hello from media-mcp-server!")
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    import asyncio
+    path = asyncio.run(upload_image('C:\\Users\\sdj21\\Pictures\\76c83370-barriers-to-genai-adoption_boomi-1024x688.jpg'))
+    print(path)
