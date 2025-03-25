55896a77b8bc772ea976ebe2d42095f44097c63e
rocksun <daijun@gmail.com>
2025-03-25 21:51:28 +0800

    add upload

--- src/media_mcp_server/server.py
+++ src/media_mcp_server/server.py
@@ -5,9 +5,25 @@ from mcp.server.fastmcp import FastMCP
 # Initialize FastMCP server
 mcp = FastMCP("media-mcp-server")
 
+from urllib.parse import urlparse
+import os
+import cloudinary
+from cloudinary import uploader
+
+# 配置Cloudinary
+cloudinary.config(
+  cloudinary_url=os.environ['CLOUDINARY_URL']
+)
+
 @mcp.tool()
 async def upload_image(image_path: str) -> str:
-    return "Image uploaded successfully"
+    upload_result = uploader.upload(
+        image_path,
+        transformation=[
+            {'quality': 'auto'}
+        ]
+    )
+    return upload_result['secure_url']
 
     # url = f"{NWS_API_BASE}/alerts/active/area/{state}"
     # data = await make_nws_request(url)
